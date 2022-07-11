ca4959ab3fdb30bccfca608db3ae26f1baa75ca5
Raghav Kansal <raghav.kansal@globant.com>
2022-07-11 14:18:40 +0530

    Test Cases for RepoImpl

--- data/src/test/java/com/example/data/repository/MarvelRepoImplTests.kt
+++ data/src/test/java/com/example/data/repository/MarvelRepoImplTests.kt
@@ -4,11 +4,21 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.common.Constant
 import com.example.data.MainCoroutineRule
 import com.example.data.remote.NetworkApi
+import com.example.data.remote.dto.characterDetail.CharacterDetailDto
+import com.example.data.remote.dto.characterList.CharacterListDto
+import com.example.data.remote.dto.characterList.Data
+import com.example.data.remote.dto.characterList.Result
+import com.example.data.remote.dto.characterList.Thumbnail
+import com.example.data.remote.utils.SafeApiRequest
+import com.example.domain.model.ModelCharacter
+import com.example.domain.model.ModelCharacterDetail
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,6 +38,9 @@ class MarvelRepoImplTests {
     @MockK
     private lateinit var networkApi: NetworkApi
 
+    @MockK
+    private lateinit var safeApiRequest: SafeApiRequest
+
     @Before
     fun setUp() {
         MockKAnnotations.init(this, relaxed = true)
@@ -35,7 +48,7 @@ class MarvelRepoImplTests {
     }
 
     @Test
-    fun `testCharacterListCalled`()=
+    fun `testCharacterListCalled`() =
         runTest {
             repoImpl.getCharactersList(Constant.paginatedValue)
             coVerify {
@@ -52,5 +65,273 @@ class MarvelRepoImplTests {
             }
         }
 
+    @Test
+    fun `testCharacterListRepoDescription`() =
+        runTest {
 
+            val actualResponse = CharacterListDto(
+                Data(
+                    listOf(
+                        Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersList(offset = Constant.paginatedValue.toString()) } } returns actualResponse
+
+            val mockResponse: ModelCharacter =
+                repoImpl.getCharactersList(offset = Constant.paginatedValue).first()
+
+            Assert.assertEquals(
+                mockResponse.description,
+                actualResponse.data.results.first().description
+            )
+        }
+
+    @Test
+    fun `testCharacterListRepoName`() =
+        runTest {
+
+            val actualResponse = CharacterListDto(
+                Data(
+                    listOf(
+                        Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersList(offset = Constant.paginatedValue.toString()) } } returns actualResponse
+
+            val mockResponse: ModelCharacter =
+                repoImpl.getCharactersList(offset = Constant.paginatedValue).first()
+
+            Assert.assertEquals(
+                mockResponse.name,
+                actualResponse.data.results.first().name
+            )
+        }
+
+    @Test
+    fun `testCharacterListRepoId`() =
+        runTest {
+
+            val actualResponse = CharacterListDto(
+                Data(
+                    listOf(
+                        Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersList(offset = Constant.paginatedValue.toString()) } } returns actualResponse
+
+            val mockResponse: ModelCharacter =
+                repoImpl.getCharactersList(offset = Constant.paginatedValue).first()
+
+            Assert.assertEquals(
+                mockResponse.id,
+                actualResponse.data.results.first().id
+            )
+        }
+
+    @Test
+    fun `testCharacterListRepoThumbnail`() =
+        runTest {
+
+            val actualResponse = CharacterListDto(
+                Data(
+                    listOf(
+                        Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersList(offset = Constant.paginatedValue.toString()) } } returns actualResponse
+
+            val mockResponse: ModelCharacter =
+                repoImpl.getCharactersList(offset = Constant.paginatedValue).first()
+
+            Assert.assertEquals(
+                mockResponse.thumbnail,
+                actualResponse.data.results.first().thumbnail.path
+            )
+        }
+
+    @Test
+    fun `testCharacterListRepoExt`() =
+        runTest {
+
+            val actualResponse = CharacterListDto(
+                Data(
+                    listOf(
+                        Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersList(offset = Constant.paginatedValue.toString()) } } returns actualResponse
+
+            val mockResponse: ModelCharacter =
+                repoImpl.getCharactersList(offset = Constant.paginatedValue).first()
+
+            Assert.assertEquals(
+                mockResponse.thumbnailExt,
+                actualResponse.data.results.first().thumbnail.extension
+            )
+        }
+
+    @Test
+    fun `testCharacterDetailsRepoId`() =
+        runTest {
+
+            val actualResponse = CharacterDetailDto(
+                com.example.data.remote.dto.characterDetail.Data(
+                    listOf(
+                        com.example.data.remote.dto.characterDetail.Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            com.example.data.remote.dto.characterDetail.Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersDetails(characterId = Constant.ID_1) } } returns actualResponse
+
+            val mockResponse: ModelCharacterDetail =
+                repoImpl.getCharactersDetails(charId = Constant.ID_1)
+
+            Assert.assertEquals(
+                mockResponse.id,
+                actualResponse.data.results.first().id
+            )
+        }
+
+    @Test
+    fun `testCharacterDetailsRepoName`() =
+        runTest {
+
+            val actualResponse = CharacterDetailDto(
+                com.example.data.remote.dto.characterDetail.Data(
+                    listOf(
+                        com.example.data.remote.dto.characterDetail.Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            com.example.data.remote.dto.characterDetail.Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersDetails(characterId = Constant.ID_1) } } returns actualResponse
+
+            val mockResponse: ModelCharacterDetail =
+                repoImpl.getCharactersDetails(charId = Constant.ID_1)
+
+            Assert.assertEquals(
+                mockResponse.name,
+                actualResponse.data.results.first().name
+            )
+        }
+
+    @Test
+    fun `testCharacterDetailsRepoDescription`() =
+        runTest {
+
+            val actualResponse = CharacterDetailDto(
+                com.example.data.remote.dto.characterDetail.Data(
+                    listOf(
+                        com.example.data.remote.dto.characterDetail.Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            com.example.data.remote.dto.characterDetail.Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersDetails(characterId = Constant.ID_1) } } returns actualResponse
+
+            val mockResponse: ModelCharacterDetail =
+                repoImpl.getCharactersDetails(charId = Constant.ID_1)
+
+            Assert.assertEquals(
+                mockResponse.description,
+                actualResponse.data.results.first().description
+            )
+        }
+
+    @Test
+    fun `testCharacterDetailsRepoThumbnail`() =
+        runTest {
+
+            val actualResponse = CharacterDetailDto(
+                com.example.data.remote.dto.characterDetail.Data(
+                    listOf(
+                        com.example.data.remote.dto.characterDetail.Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            com.example.data.remote.dto.characterDetail.Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersDetails(characterId = Constant.ID_1) } } returns actualResponse
+
+            val mockResponse: ModelCharacterDetail =
+                repoImpl.getCharactersDetails(charId = Constant.ID_1)
+
+            Assert.assertEquals(
+                mockResponse.thumbnail,
+                actualResponse.data.results.first().thumbnail.path
+            )
+        }
+
+    @Test
+    fun `testCharacterDetailsRepoThumbnailExt`() =
+        runTest {
+
+            val actualResponse = CharacterDetailDto(
+                com.example.data.remote.dto.characterDetail.Data(
+                    listOf(
+                        com.example.data.remote.dto.characterDetail.Result(
+                            Constant.Best_Avenger,
+                            Constant.ID_1,
+                            Constant.Iron_Man,
+                            com.example.data.remote.dto.characterDetail.Thumbnail(Constant.IMG_JPG, Constant.Iron_Man_Image)
+                        )
+                    )
+                )
+            )
+            coEvery { safeApiRequest.safeApiRequest { networkApi.getCharactersDetails(characterId = Constant.ID_1) } } returns actualResponse
+
+            val mockResponse: ModelCharacterDetail =
+                repoImpl.getCharactersDetails(charId = Constant.ID_1)
+
+            Assert.assertEquals(
+                mockResponse.thumbnailExt,
+                actualResponse.data.results.first().thumbnail.extension
+            )
+        }
 }
